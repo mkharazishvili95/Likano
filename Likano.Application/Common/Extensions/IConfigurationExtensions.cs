8b7c11d1c8c0951a81cb9560168612b4b0214b6e
mikheil <mikheil@lemondo.com>
2026-01-17 16:46:14 +0400

    Added QueriesBase for using Ado.net clearly

---
+++ Likano.Application/Common/Extensions/IConfigurationExtensions.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.Extensions.Configuration;
+
+namespace Likano.Application.Common.Extensions
+{
+    public static class IConfigurationExtensions
+    {
+        public static string GetReadOnlyConnectionString(this IConfiguration configuration)
+        {
+            return configuration.GetConnectionString("DefaultConnection") ?? throw new ArgumentException();
+        }
+    }
+}
---
+++ Likano.Infrastructure/Queries/QueriesBase.cs
@@ -0,0 +1,67 @@
+﻿using Likano.Application.Common.Extensions;
+using Microsoft.Data.SqlClient;
+using Microsoft.Extensions.Configuration;
+
+namespace Likano.Infrastructure.Queries
+{
+    public abstract class QueriesBase
+    {
+        string connectionString { get; set; }
+        public QueriesBase(IConfiguration configuration)
+        {
+            connectionString = configuration.GetReadOnlyConnectionString();
+        }
+        public async Task<TResponse> Get<TResponse>(string commandText, Func<SqlDataReader, TResponse> selector) where TResponse : new()
+        {
+            var response = new TResponse();
+            using (var connection = new SqlConnection(connectionString))
+            {
+                await connection.OpenAsync();
+
+                using (SqlCommand command = connection.CreateCommand())
+                {
+                    command.CommandText = commandText;
+                    using var reader = await command.ExecuteReaderAsync();
+                    if (reader.HasRows)
+                        while (await reader.ReadAsync())
+                        {
+                            response = await reader.Select(selector);
+                        }
+                }
+                await connection.CloseAsync();
+            }
+            return response;
+        }
+
+        public async Task<List<TResponse>> GetMany<TResponse>(string commandText, Func<SqlDataReader, TResponse> selector)
+        {
+            var response = new List<TResponse>();
+            using (var connection = new SqlConnection(connectionString))
+            {
+                await connection.OpenAsync();
+
+                using (SqlCommand command = connection.CreateCommand())
+                {
+                    command.CommandText = commandText;
+                    using var reader = await command.ExecuteReaderAsync();
+                    if (reader.HasRows)
+                        while (await reader.ReadAsync())
+                        {
+                            var value = await reader.Select(selector);
+                            response.Add(value);
+                        }
+                }
+                await connection.CloseAsync();
+            }
+            return response;
+        }
+    }
+
+    public static class QuerieHelpler
+    {
+        public static async Task<T> Select<T>(this SqlDataReader reader, Func<SqlDataReader, T> selector)
+        {
+            return selector.Invoke(reader);
+        }
+    }
+}
