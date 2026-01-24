f6d41804b3ced4bbdefc789b8efde5d4d553ba14
mikheil <mikheil@lemondo.com>
2026-01-24 11:48:44 +0400

    Added UrlGenerator for seo

---
+++ Likano.Application/Helpers/UrlGenerator.cs
@@ -0,0 +1,27 @@
+﻿namespace Likano.Application.Helpers
+{
+    public static class UrlGenerator
+    {
+        private static readonly string geo = "აბგდევზთიკლმნოპჟრსტუფქღყშჩცძწჭხჯჰ";
+        private static readonly string[] lat = "a-b-g-d-e-v-z-t-i-k-l-m-n-o-p-zh-r-s-t-u-f-q-gh-y-sh-ch-c-dz-ts-tch-x-j-h".Split('-');
+
+        public static string Transliterate(string? text)
+        {
+            var result = "";
+            if (!string.IsNullOrWhiteSpace(text))
+            {
+                foreach (var ch in text.Trim())
+                {
+                    var index = geo.IndexOf(ch);
+                    result += index != -1 ? lat[index] : ch.ToString();
+                }
+                result = result.Replace(' ', '-');
+                result = result.Replace('&', '-');
+                result = result.Replace('/', '-');
+                result = result.Replace('\\', '-');
+                result = result.Replace('#', '-');
+            }
+            return result;
+        }
+    }
+}
