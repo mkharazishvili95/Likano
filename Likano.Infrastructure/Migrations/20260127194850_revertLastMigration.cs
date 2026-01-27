d9736ef9ab8b80776d5ff05979e6084fde355a82
mikheil <mikheil@lemondo.com>
2026-01-27 23:49:23 +0400

    sd

---
+++ Likano.Infrastructure/Migrations/20260127194850_revertLastMigration.cs
@@ -0,0 +1,28 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace Likano.Infrastructure.Migrations
+{
+    /// <inheritdoc />
+    public partial class revertLastMigration : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "IncludedItems",
+                table: "Products");
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<string>(
+                name: "IncludedItems",
+                table: "Products",
+                type: "nvarchar(max)",
+                nullable: true);
+        }
+    }
+}
