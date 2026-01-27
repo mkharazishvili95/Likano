c9f5660c895541c00d11b6fe22d85465211f2f1f
mikheil <mikheil@lemondo.com>
2026-01-27 23:36:17 +0400

    added IncludedItemsIntoProduct/ Added migration

---
+++ Likano.Infrastructure/Migrations/20260127193450_AddedIncludedItemsIntoProduct.cs
@@ -0,0 +1,28 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace Likano.Infrastructure.Migrations
+{
+    /// <inheritdoc />
+    public partial class AddedIncludedItemsIntoProduct : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<string>(
+                name: "IncludedItems",
+                table: "Products",
+                type: "nvarchar(max)",
+                nullable: true);
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "IncludedItems",
+                table: "Products");
+        }
+    }
+}
